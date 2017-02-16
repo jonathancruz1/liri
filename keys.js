45d024fb381a6c9d1d39b0c8808f91df9312f781
VIA <VIA@BES-Macbook-JRoth.home>
2017-02-16 00:42:26 -0500

    Adding keys.js

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'PHXoRBjBvn3k4pqmdwniymEUz',
+  consumer_secret: '3UkHGuzSK7cHGGpBKC1qGf28DH4lFu6IO6yJfHLBxgP2kbZ72Z',
+  access_token_key: '297432027-4CGrzG4ogwrHE8uj1EshpyUHdG7WiBc1qWy4WyMz',
+  access_token_secret: 'os7VA2HWtKh6G8JSmUNSntUIIpwMik4QxPCPwOXeTQyvQ',
+}
